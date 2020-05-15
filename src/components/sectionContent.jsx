82c9aace762576cc7b67e88ca052539a632c6151
lifecoder <lifecoder.ua@gmail.com>
2020-05-15 14:38:12 +0300

    Updates

--- src/components/sectionContent.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-const SectionContent = ({entry}) => (
-  <div dangerouslySetInnerHTML={{ __html: entry.html }} />
-);
-
-export default SectionContent;
---
+++ styles-6e8d16f0e6e0cbc3218d.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{"8ypT":function(n,o,p){},"J+8R":function(n,o,p){},emhx:function(n,o,p){}}]);
+//# sourceMappingURL=styles-6e8d16f0e6e0cbc3218d.js.map
