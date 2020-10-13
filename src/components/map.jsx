2ea35d225fa35277e44eef712f88ab79ea825e93
lifecoderua <lifecoder.ua@gmail.com>
2020-10-13 19:26:42 +0300

    make map width dynamic

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -3,7 +3,7 @@ import React from "react"
 const Map = () => (
     <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1338.4558794155262!2d35.099384990924264!3d47.86067219761604!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40dc66c58e768609%3A0x8fbb210086b123e4!2z0L_RgNC-0YHQvy4g0KHQvtCx0L7RgNC90YvQuSwgMjI4LCDQl9Cw0L_QvtGA0L7QttGM0LUsINCX0LDQv9C-0YDQvtC20YHQutCw0Y8g0L7QsdC70LDRgdGC0YwsIDY5MDAw!5e0!3m2!1sru!2sua!4v1602605787987!5m2!1sru!2sua"
-        width="600" height="450" frameBorder="0" style={{ border: 0, }} allowFullScreen="" aria-hidden="false"
+        width="600" height="450" frameBorder="0" style={{ border: 0, width: "100%", }} allowFullScreen="" aria-hidden="false"
         tabIndex="0"></iframe>
 );
 
